7ebc9debac6bc278f42d6f324ed02ecef579c786
Dikksap <dikkhidayat@gmail.com>
2026-09-13 18:52:48 +0700

    fix-frontend(enviroment):fix admin refresh error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,36 +3,40 @@ import babel from "@rolldown/plugin-babel";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/",
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
-    // `npm run dev:mobile` -> HTTPS agar getUserMedia kamera aktif di HP
+
     ...(mode === "mobile" ? [basicSsl()] : []),
   ],
+
   build: {
     outDir: "dist/app",
     emptyOutDir: true,
+
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          if (id.includes('xlsx')) {
-            return 'xlsx';
+          if (id.includes("xlsx")) {
+            return "xlsx";
           }
-          if (id.includes('node_modules')) {
-            return 'vendor';
+
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
-        }
-      }
-    }
+        },
+      },
+    },
   },
+
   optimizeDeps: {
     include: ["xlsx"],
   },
+
   server: {
     port: 5173,
     strictPort: true,
